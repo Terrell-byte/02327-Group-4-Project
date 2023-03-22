6c957bfcff6232b1d044655cd0ef05c8e97af520
Anders Nørgaard <ansi1806@gmail.com>
2023-03-22 10:03:18 +0100

    good table

--- docker/4_02327DatabaseScript1_2023.sql
+++ docker/4_02327DatabaseScript1_2023.sql
@@ -2,13 +2,24 @@ USE gruppe4;
 
 DROP TABLE IF EXISTS Phone;
 DROP TABLE IF EXISTS Email;
-DROP TABLE IF EXISTS Journalist;
 DROP TABLE IF EXISTS Reference;
 DROP TABLE IF EXISTS Footage;
 DROP TABLE IF EXISTS Edition;
-DROP TABLE IF EXISTS Topic;
 DROP TABLE IF EXISTS Item;
+DROP TABLE IF EXISTS Topic;
+DROP TABLE IF EXISTS Journalist;
 
+
+CREATE TABLE Journalist (
+    CPR CHAR(10) primary key,
+    FirstName varchar(20),
+    LastName varchar(20),
+    Address varchar(30),
+    Civic varchar(5),
+    City varchar(20),
+    ZIP char(4),
+    Country char(2)
+);
 
 CREATE TABLE Phone (
     JournalistCPR char(10) primary key,
@@ -22,29 +33,23 @@ CREATE TABLE Email (
     FOREIGN KEY (JournalistCPR) references Journalist(CPR)
 );
 
-CREATE TABLE Journalist (
-    CPR CHAR(10) primary key,
-    FirstName varchar(20),
-    LastName varchar(20),
-    Address varchar(30),
-    Civic varchar(5),
-    City varchar(20),
-    ZIP char(4),
-    Country char(2)
+CREATE TABLE Topic (
+   Title varchar(40) Primary key,
+   Description varchar(255)
 );
 
 CREATE TABLE Reference (
     TopicTitle varchar(40),
     JournalistCPR CHAR(10),
     Role varchar(20),
-    constraint referenceID primary key(TopicTitle, JournalistCPR),
     FOREIGN KEY (JournalistCPR) references Journalist(CPR),
-    foreign key (TopicTitle) references Topic(Title)
+    foreign key (TopicTitle) references Topic(Title),
+    constraint referenceID primary key(TopicTitle, JournalistCPR)
 );
 
 CREATE TABLE Footage (
-    Title varchar(40) Primary key,
-    DateShot date primary key,
+    Title varchar(40),
+    DateShot date,
     DurationLength int,
     ReportCPR CHAR(10),
     constraint FootageID primary key(Title, DateShot)
@@ -53,13 +58,10 @@ CREATE TABLE Footage (
 CREATE TABLE Edition (
     StartDate dateTime Primary key,
     EndDate dateTime,
-    HostCPR CHAR(10)
+    HostCPR CHAR(10),
+    Foreign key (HostCPR) references Journalist(CPR)
 );
 
-CREATE TABLE Topic (
-   Title varchar(40) Primary key,
-   Description varchar(255)
-);
 
 CREATE TABLE Item (
     DateTimeSlot dateTime primary key,

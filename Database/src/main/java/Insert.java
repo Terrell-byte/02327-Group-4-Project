9f9d4dd7f428a307aa8b0c50acdc2b217f50075c
Terrell-byte <danieltsutton@outlook.com>
2023-03-27 18:15:09 +0200

    testing inserting into DBMS with java

---
+++ Database/src/main/java/Insert.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class Insert {
+    void insert() {
+        String url = "jdbc:mariadb://localhost:5041/";
+        String driver = "org.mariadb.jdbc.Driver";
+        String userName = "root";
+        String password = "rootpassword";
+        try {
+            Class.forName(driver).newInstance();
+            Connection conn = DriverManager.getConnection(url, userName, password);
+            Statement st = conn.createStatement();
+            st.executeUpdate("INSERT INTO test VALUES (1, 'test')");
+            st.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+

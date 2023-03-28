4d2d2e3ee1355373f6de9acea784a7d9a0477926
Matthias Kobbernagel <Skumfidusendesigns@gmail.com>
2023-03-28 09:27:11 +0200

    Connection to database now works

--- Database/src/main/java/Insert.java
+++ Database/src/main/java/Insert.java
@@ -2,19 +2,23 @@ import java.sql.*;
 
 public class Insert {
     void insert() {
-        String url = "jdbc:mariadb://localhost:5041/";
-        String driver = "org.mariadb.jdbc.Driver";
         String userName = "root";
         String password = "rootpassword";
+        String url = "jdbc:mysql://127.0.0.1:5041/gruppe4";
+
         try {
-            Class.forName(driver).newInstance();
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
             Connection conn = DriverManager.getConnection(url, userName, password);
             Statement st = conn.createStatement();
-            st.executeUpdate("INSERT INTO test VALUES (1, 'test')");
+
+            //st.executeUpdate("DROP TABLE test");
+            st.executeUpdate("CREATE TABLE test (name VARCHAR(20))");
+            //st.execute("INSERT INTO test VALUES (1, 'test')");
             st.close();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
+}
 
 
--- Database/src/main/java/LoaderExample.java
+++ Database/src/main/java/LoaderExample.java
@@ -4,12 +4,15 @@ import java.util.List;
 public class LoaderExample {
 
 	public static void main(String[] args) {
+		//Insert connection = new Insert();
+		//connection.insert();
+
 		FootagesAndReportersLoader loader = new FootagesAndReportersLoader();
 		try {
 			System.out.println("loading from "+ "uploads.csv");
 			List<FootageAndReporter> footagesAndReporters = loader.loadFootagesAndReporters("uploads.csv");
 			for(FootageAndReporter footageAndReporter : footagesAndReporters) {
-				System.out.print("\tFootage: " + footageAndReporter.getFootage());
+				System.out.println("\tFootage: " + footageAndReporter.getFootage());
 				System.out.println("\tReporter: " + footageAndReporter.getReporter());
 			}
 		} catch (IOException e) {
